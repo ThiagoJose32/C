ee5ddde95ec85a57cebf3fc2a7c9fb34c4c48987
Thiago José <96024152+ThiagoJose32@users.noreply.github.com>
2022-04-11 14:59:30 -0300

    Update main.c

--- ESTD-Aula-2-Ponteiros/Q5/main.c
+++ ESTD-Aula-2-Ponteiros/Q5/main.c
@@ -1,10 +1,20 @@
 /*
 5) Acrescente uma função no exercício anterior que receba o vetor como parâmetro e retorne a quantidade de valores positivos.
+  ( 4) Faça uma programa que declare um vetor de inteiros com 5 elementos e implemente uma função para les os números e outra para imprimir os valores;)
 */
 
 #include <stdio.h>
 
+void lerVetor(int vet[5]){
+  int i;
+  for(i=0;i<5;i++){
+    printf("Informe o valor: ");
+    scanf("%d", &vet[i]);  //(vet+1)
+  }
+}
+
 int main(void) {
-  printf("Hello World\n");
-  return 0;
+  int vetor[5];
+  lerVetor(vetor);
+  imprimir(vetor);
 }
