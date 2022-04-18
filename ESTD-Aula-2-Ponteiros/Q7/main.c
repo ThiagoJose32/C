691a2811feba3aecf979aa261770f77f59bd5311
Thiago José <96024152+ThiagoJose32@users.noreply.github.com>
2022-04-18 08:22:57 -0300

    Create main.c

---
+++ ESTD-Aula-2-Ponteiros/Q7/main.c
@@ -0,0 +1,20 @@
+/*
+Faça um programa que declare um vetor de 10 posições, do tipo int, float ou double. Implemente funções:
+• Para ler os valores do vetor;
+• Para mostrar os valores do vetor;
+• Para exibir os valores maiores que um valor N fornecido pelo usuário;
+• Para calcular a media dos valores pares do vetor.
+• Que receba o vetor e os endereços de duas
+variáveis inteiras, digamos min e max, e deposite
+nessas variáveis o valor de um elemento mínimo e o valor de um elemento máximo
+do vetor.
+
+Observação: não usar o vetor como variável global.
+*/
+
+#include <stdio.h>
+
+int main(void) {
+  printf("Hello World\n");
+  return 0;
+}
