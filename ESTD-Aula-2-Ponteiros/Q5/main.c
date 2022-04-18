37bd3adba558fd04b8c5d2c99119b59d2bac6e9d
Thiago José <96024152+ThiagoJose32@users.noreply.github.com>
2022-04-18 07:51:40 -0300

    Finalizada

--- ESTD-Aula-2-Ponteiros/Q5/main.c
+++ ESTD-Aula-2-Ponteiros/Q5/main.c
@@ -6,15 +6,33 @@
 #include <stdio.h>
 
 void lerVetor(int vet[5]){
-  int i;
-  for(i=0;i<5;i++){
+  int cont;
+  for(cont=0;cont<5;cont++){
     printf("Informe o valor: ");
-    scanf("%d", &vet[i]);  //(vet+1)
+    scanf("%d", &vet[cont]);  //(vet+1)
   }
+}
+
+void imprimir(int vet[5]){
+  int cont;
+  for(cont=0;cont<5;cont++){
+    printf("\n\nVet[%d] = %d", cont, vet[cont]);
+  }
+}
+
+int positivos(int vet[5]){
+  int cont, qtdPositivos=0;
+  for(cont=0;cont<5;cont++){
+    if(vet[cont]>0){
+      qtdPositivos++;
+    }
+  }
+  return qtdPositivos;
 }
 
 int main(void) {
   int vetor[5];
   lerVetor(vetor);
   imprimir(vetor);
+  printf("\n\nExistem %d números posivitos neste vetor.", positivos(vetor));
 }
