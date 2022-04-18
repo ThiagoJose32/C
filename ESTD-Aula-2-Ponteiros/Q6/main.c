094a577f09dffe1d58068ee2d0e117aa496d8f79
Thiago José <96024152+ThiagoJose32@users.noreply.github.com>
2022-04-18 07:52:31 -0300

    Finalizada

--- ESTD-Aula-2-Ponteiros/Q6/main.c
+++ ESTD-Aula-2-Ponteiros/Q6/main.c
@@ -7,7 +7,45 @@ Imprima o retorno das funções acima na função main.
 
 #include <stdio.h>
 
+void lerVetor(int vet[5]){
+  int cont;
+  for(cont=0;cont<5;cont++){
+    printf("Informe o valor: ");
+    scanf("%d", &vet[cont]);  //(vet+1)
+  }
+}
+
+void imprimir(int vet[5]){
+  int cont;
+  for(cont=0;cont<5;cont++){
+    printf("\n\nVet[%d] = %d", cont, vet[cont]);
+  }
+}
+
+int positivos(int vet[5]){
+  int cont, qtdPositivos=0;
+  for(cont=0;cont<5;cont++){
+    if(vet[cont]>0){
+      qtdPositivos++;
+    }
+  }
+  return qtdPositivos;
+}
+
+int pares(int vet[5]){
+  int cont, qtdPares=0;
+  for(cont=0;cont<5;cont++){
+    if(vet[cont] % 2 == 0){
+      qtdPares++;
+    }
+  }
+  return qtdPares;
+}
+
 int main(void) {
-  printf("Hello World\n");
-  return 0;
+  int vetor[5];
+  lerVetor(vetor);
+  imprimir(vetor);
+  printf("\n\nExistem %d números positivos neste vetor.", positivos(vetor));
+  printf("\n\nExistem %d números pares neste vetor.", pares(vetor));
 }
