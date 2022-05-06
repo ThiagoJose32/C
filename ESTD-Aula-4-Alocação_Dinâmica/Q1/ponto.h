c81f9c8cd4999145f12c567db80b02335d8a15b3
Thiago José <96024152+ThiagoJose32@users.noreply.github.com>
2022-05-06 20:38:05 -0300

    Create ponto.h

---
+++ ESTD-Aula-4-Alocação_Dinâmica/Q1/ponto.h
@@ -0,0 +1,13 @@
+struct ponto{
+  int x;
+  int y;
+};
+typedef struct ponto Ponto;
+
+Ponto* criar(int x, int y);
+
+void imprimir(Ponto *p);
+
+void liberar(Ponto *p);
+
+//float calcularDistancia(Ponto *p1, Ponto *p2);
