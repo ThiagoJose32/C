c60134bf40e60d02380f9d89bab9178ea05d1d88
Thiago José <96024152+ThiagoJose32@users.noreply.github.com>
2022-05-07 16:10:14 -0300

    Função transladar adicionada
    b - acrescente uma função para transladar um ponto para uma outra posição (receber valores x1 e y1 a serem acrescentados num ponto p);

--- ESTD-Aula-4-Alocação_Dinâmica/Q1/main.c
+++ ESTD-Aula-4-Alocação_Dinâmica/Q1/main.c
@@ -4,12 +4,37 @@
 
 
 int main(void) {
-  int x, i;
-
+  char aux;
+  int i=1;
+  
   Ponto *p1 = criar(2, 5);
   Ponto *p2 = criar(-5, -2);
-
+  Ponto *p3 = criar(10, -10);
+  
   imprimir(p1);
   imprimir(p2);
   printf("\nDistancia: %.2f", calcularDistancia(p1,p2));
+
+  printf("\n\nAlterar ponto A ou B? ");
+  
+  while(i==1){
+    scanf("%c", &aux);
+    switch(aux){
+      case 'A':
+        transladar(p1);
+        printf("\nNova localização para A: [%d;%d]", p1->x, p1->y);
+        i++;
+      break;
+
+      case 'B':
+        transladar(p2);
+        printf("\nNova localização para B: [%d;%d]", p2->x, p2->y);
+        i++;
+      break;
+
+      default:
+        printf("É A ou B só: ");
+    }
+    getc(stdin);
+ }
 }
